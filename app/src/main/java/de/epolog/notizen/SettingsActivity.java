4a1e6cf7dac93c2683e34484bfeb66df77a28b9f
RnBCoder <crazytunes@live.de>
2016-11-06 14:34:02 +0100

    added switch

--- app/src/main/java/de/epolog/notizen/SettingsActivity.java
+++ app/src/main/java/de/epolog/notizen/SettingsActivity.java
@@ -1,8 +1,8 @@
 package de.epolog.notizen;
 
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import java.util.Calendar;
@@ -14,39 +14,41 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        int start = 8;
-        int end = 20;
-        int hours = (end - start) % 24;
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, start);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-
-        long startHourMilli = cal.getTimeInMillis();
-
-        cal.add(Calendar.HOUR_OF_DAY, hours);
-
-        long endHourMilli = cal.getTimeInMillis();
+    }
 
 
-        long currentMilli = System.currentTimeMillis();
-
-        if (currentMilli >= startHourMilli && currentMilli < endHourMilli) {
-            View someView = findViewById(R.id.activity_settings);
-            View root = someView.getRootView();
-            root.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
-        } else {
-            View someView = findViewById(R.id.activity_settings);
-            View root = someView.getRootView();
-            root.setBackgroundColor(ContextCompat.getColor(this, R.color.grey));
-        }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getDelegate().onStart();
 
 
+        int start = 20;
+        int end = 8;
+
+        this.dayNightMode(Calendar.getInstance(), start, end);
+    }
+
+    private void dayNightMode(Calendar cal, int startTime, int endTime){
+        int now = cal.get(Calendar.HOUR_OF_DAY);
+        if(now >= startTime || now < endTime)
+            this.setColours(ContextCompat.getColor(this, R.color.grey), ContextCompat.getColor(this, R.color.white));
+        else
+            this.setColours(ContextCompat.getColor(this, R.color.white), ContextCompat.getColor(this, R.color.black));
+    }
+
+    private void setColours(int bc, int tc){
+        View someView = findViewById(R.id.activity_settings);
+        View root = someView.getRootView();
+        root.setBackgroundColor(bc);
     }
 
 
 
-
 }
+
+
+
+
+
+
